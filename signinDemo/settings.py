af42d15a2c52b52ceb0fa40bb9a15f277001e42b
Akshat <user@Akshats-MB-Pro-15.local>
2022-07-19 01:38:06 +0530

    added secret key

--- signinDemo/settings.py
+++ signinDemo/settings.py
@@ -21,6 +21,8 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = env('SECRET_KEY')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
