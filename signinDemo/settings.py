2f56206bc97b92f65f54e0f1fd2169dee2631dc4
Akshat <user@Akshats-MB-Pro-15.local>
2022-07-19 01:42:29 +0530

    env

--- signinDemo/settings.py
+++ signinDemo/settings.py
@@ -15,11 +15,11 @@ import environ
 import dj_database_url
 import os
 
-env = environ.Env()
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env()
 
 SECRET_KEY = env('SECRET_KEY')
 
